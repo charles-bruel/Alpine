e18ff36f58da0c7e9abbcce692ef7907db4dd2f0
charles-bruel <csbruel@gmail.com>
2023-01-04 06:46:48 -0500

    Basic 2D camera UI support, basic move script Changed layers so it only sees 2D stuff Very little 2d stuff implemented so far Zoom feature is TODO

--- Assets/Scripts/UI/LayersController.cs
+++ Assets/Scripts/UI/LayersController.cs
@@ -5,10 +5,18 @@ using UnityEngine.UI;
 
 public class LayersController : MonoBehaviour
 {
+    public Camera MainCamera;
+    public Camera TopCamera;
+
     public void OnContoursClicked(Toggle change) {
         List<TerrainTile> tiles = TerrainManager.Instance.Tiles;
         for(int i = 0;i < tiles.Count;i ++) {
             tiles[i].ContoursComponent.gameObject.SetActive(change.isOn);
         }
+    }
+
+    public void OnModeChangeClicked(Toggle change) {
+        MainCamera.gameObject.SetActive(!change.isOn);
+        TopCamera.gameObject.SetActive(change.isOn);
     }
 }
