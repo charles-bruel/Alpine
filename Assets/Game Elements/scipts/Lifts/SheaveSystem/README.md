f67073f5eee891f616ceae98cc8e3277b7d34be0
charles-bruel <csbruel@gmail.com>
2023-01-06 16:43:23 -0500

    Added sheave system

---
+++ Assets/Game Elements/scipts/Lifts/SheaveSystem/README.md
@@ -0,0 +1,3 @@
+# Sheave System
+
+This was taken 1:1 from the avalanche sheave system, though it will now deviate from it.
---
+++ Assets/Game Elements/scipts/Lifts/SheaveSystem/TowerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TowerScript : MonoBehaviour
+{
+    public float Width;
+    public GameObject PaddingSource;
+    public List<GameObject> PaddingObjects;
+    public Transform BaseItems;
+    public Transform Target;
+    public Transform WirePoint;
+
+    void Update()
+    {
+        //Ugly and possibly slow but gets the job done.
+        if (PaddingSource != null)
+        {
+            foreach (GameObject obj in PaddingObjects){
+                if(obj.activeSelf != PaddingSource.activeSelf)
+                    obj.SetActive(PaddingSource.activeSelf);
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        if(BaseItems != null)
+        {
+            RaycastHit hitInfo = default(RaycastHit);
+            Physics.Raycast(transform.position, Target.position - transform.position, out hitInfo, float.MaxValue, 768);
+            BaseItems.position = hitInfo.point;
+        }
+    }
+}
