5432c62726e98a410a607bed210743f7d7e221fa
charles-bruel <csbruel@gmail.com>
2023-02-10 18:18:06 -0500

    Midstation speed change

--- Assets/Game Elements/scipts/Lifts/MidStationT3.cs
+++ Assets/Game Elements/scipts/Lifts/MidStationT3.cs
@@ -87,8 +87,15 @@ public class MidStationT3 : APITurnSegment {
     private List<LiftCablePoint> GetCablePointsUniversal(GameObject self, bool uphill) {
         Transform primarySegment = self.transform.GetChild(IntParameters[0]);
         Transform secondarySegment = self.transform.GetChild(IntParameters[1]);
-        Vector3 primarySegmentCP = primarySegment.GetChild(IntParameters[uphill ? 3 : 4]).position;
-        Vector3 secondarySegmentCP = secondarySegment.GetChild(IntParameters[uphill ? 3 : 4]).position;
+
+        Transform primarySegmentCPTransform = primarySegment.GetChild(IntParameters[uphill ? 3 : 4]);
+        Transform secondarySegmentCPTransform = secondarySegment.GetChild(IntParameters[uphill ? 3 : 4]);
+
+        //Grab the innermost points for start and end
+        Vector3 primarySegmentCP = primarySegmentCPTransform.GetChild(1).position;
+        Vector3 secondarySegmentCP = secondarySegmentCPTransform.GetChild(1).position;
+
+        int extraPoints = primarySegmentCPTransform.childCount + secondarySegmentCPTransform.childCount + 2;
         
         float Arad = primarySegment.eulerAngles.y * Mathf.Deg2Rad;
         Vector2 Aray = new Vector2(Mathf.Sin(Arad), Mathf.Cos(Arad));
@@ -102,14 +109,28 @@ public class MidStationT3 : APITurnSegment {
 
         Vector2 intersection = Utils.LineLine(A1, A2, B1, B2);
         
-        List<Vector2> filletResult = Utils.Fillet(A1, intersection, B1, 3, 64);
+        List<Vector2> filletResult = Utils.Fillet(A1, intersection, B1, 3, 32);
 
-        List<LiftCablePoint> toReturn = new List<LiftCablePoint>(filletResult.Count);
+        List<LiftCablePoint> toReturn = new List<LiftCablePoint>(filletResult.Count - 2 + extraPoints);
+
+        toReturn.Add(new LiftCablePoint(primarySegmentCPTransform.position, primarySegmentCPTransform.localScale.x));
+
+        for(int i = 0;i < primarySegmentCPTransform.childCount;i ++) {
+            Transform temp = primarySegmentCPTransform.GetChild(i);
+            toReturn.Add(new LiftCablePoint(temp.position, temp.localScale.x));
+        }
 
         for(int i = 0;i < filletResult.Count;i ++) {
             Vector3 pos = new Vector3(filletResult[i].x, primarySegmentCP.y, filletResult[i].y);
-            toReturn.Add(new LiftCablePoint(pos, 1));
+            toReturn.Add(new LiftCablePoint(pos, toReturn[toReturn.Count - 1].speed));
         }
+
+        for(int i = secondarySegmentCPTransform.childCount - 1;i >= 0;i --) {
+            Transform temp = secondarySegmentCPTransform.GetChild(i);
+            toReturn.Add(new LiftCablePoint(temp.position, temp.localScale.x));
+        }
+
+        toReturn.Add(new LiftCablePoint(secondarySegmentCPTransform.position, secondarySegmentCPTransform.localScale.x));
 
         if(!uphill) toReturn.Reverse();
 
