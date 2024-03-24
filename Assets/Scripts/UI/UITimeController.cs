0c699ba4bab130cb760d2d81a2383442d3b2a9cc
charles-bruel <csbruel@gmail.com>
2024-03-24 08:09:49 -0400

    Added keyboard time controls

--- Assets/Scripts/UI/UITimeController.cs
+++ Assets/Scripts/UI/UITimeController.cs
@@ -13,24 +13,68 @@ public class UITimeController : MonoBehaviour {
 
     [NonSerialized]
     public Dictionary<Toggle, float> ToggleToTimeMultiplier;
+    [NonSerialized]
+    public Dictionary<int, Toggle> IndexToToggle;
+    [NonSerialized]
+    public Dictionary<Toggle, int> ToggleToIndex;
+
+    private int LastTimeIndex = 1;
 
     private bool Initialized = false;
 
     private void Initialize() {
         if(Initialized) return;
         Initialized = true;
+        // TODO: Don't hardcode this
         ToggleToTimeMultiplier = new Dictionary<Toggle, float> {
             {X0, 0},
             {X1, 1},
             {X10, 10},
             {X100, 100}
         };
+        IndexToToggle = new Dictionary<int, Toggle> {
+            {0, X0},
+            {1, X1},
+            {2, X10},
+            {3, X100}
+        };
+        ToggleToIndex = new Dictionary<Toggle, int> {
+            {X0, 0},
+            {X1, 1},
+            {X10, 2},
+            {X100, 3}
+        };
     }
 
-    public void OnTimeToggleChanged() {
+    void Update() {
         Initialize();
-        
+
+        for(int i = 0; i < ToggleToTimeMultiplier.Count; i++) {
+            if(Input.GetKeyDown("" + i)) {
+                IndexToToggle[i].isOn = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (GameController.Instance.TimeMultiplier == 0) {
+                IndexToToggle[LastTimeIndex].isOn = true;
+            } else {
+                IndexToToggle[0].isOn = true;
+            }
+        }
+    }
+
+    public void OnTimeToggleChanged() {        
         float newTimeMultiplier = ToggleToTimeMultiplier[TimeToggleGroup.ActiveToggles().First()];
+
+        if (newTimeMultiplier != 0) {
+            foreach (var key_value in ToggleToTimeMultiplier) {
+                if (key_value.Value == GameController.Instance.TimeMultiplier) {
+                    LastTimeIndex = ToggleToIndex[key_value.Key];
+                }
+            }
+        }
+
         // It can be null right on startup
         if(GameController.Instance != null) {
             GameController.Instance.TimeMultiplier = newTimeMultiplier;
